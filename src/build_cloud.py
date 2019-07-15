bdf94047cabff19da4efe33ce8ada1942ebe760d
Tanner Poling <tannerpoling@gmail.com>
2019-07-15 16:02:33 -0700

    metashape script working, trying different levels of quality in rendering

--- src/build_cloud.py
+++ src/build_cloud.py
@@ -3,27 +3,35 @@
 import Metashape
 # argparse for using passed arguments
 import argparse
-
+import os
+import sys
+import time
 # This script will be passed:
 # frame_filepath as arg1, FRAME_LIMIT as arg2, frame_number as arg3
 # Since the names of each frame is just frame_number.jpg,
 # this script can use that fact to make a list of image filenames
 # to be added to the metashape project.
-# For now the idea is to have a "test" metashape project in my Documents
-# folder, which will be blank when starting and is saved every time
-# this script is called
+# Be sure to set a filepath for the metashape project file this script
+# will use and modify as it runs
 
 # Save passed arguments into variables
 frame_filepath = sys.argv[1]
-FRAME_LIMIT = sys.argv[2]
-frame_number = sys.argv[3]
+FRAME_LIMIT = int(sys.argv[2])
+frame_number = int(sys.argv[3])
+
+# User-defined filepath for the metashape project used by this script
+METASHAPE_PROJECT_PATH = "/home/tanner/code/metashape_node/project/test.psx"
 
 # Set up Metashape document and chunk
-# currently this doesn't work: there's a weird issue with Metashape
-# returning a NoneType object when using Metashape.app.document
 doc = Metashape.Document()
-doc.open("/home/tanner/code/metashape_node/project/test.psx")
-chunk = doc.chunk()
+try:
+    doc.open(METASHAPE_PROJECT_PATH)
+    chunk = doc.chunks[0]
+except OSError:
+    chunk = doc.addChunk()
+
+# GPU mask to force usage of gpu for calculations
+Metashape.app.gpu_mask = 2 ** (len(Metashape.app.enumGPUDevices())) - 1
 
 # Build list of filenames based on FRAME_LIMIT, frame_number,
 # and frame_filepath
@@ -31,16 +39,25 @@ first_frame = frame_number - FRAME_LIMIT
 last_frame = frame_number
 frame_list = []
 for current_frame_num in range(first_frame, last_frame):
-    current_frame_filepath = frame_filepath + str(current_frame_num) + ".jpeg"
+    current_frame_filepath = frame_filepath + str(current_frame_num) + ".JPG"
     frame_list.append(current_frame_filepath)
 
 # Add frame_list to chunk in metashape
 chunk.addPhotos(frame_list)
 
 # Align cameras and build dense cloud
+start_time = time.time()
+chunk.matchPhotos(accuracy=Metashape.LowestAccuracy)
+print("Photo matching took", time.time() - start_time, "to run")
 chunk.alignCameras()
+start_time = time.time()
+chunk.buildDepthMaps(quality=Metashape.LowestQuality)
+print("Depth maps took", time.time() - start_time, "to run")
 chunk.buildDenseCloud()
 
 # Save generated dense cloud to same location as frames
 cloud_filename = frame_filepath + "cloud_" + str(frame_number) + ".obj"
-exportPoints(cloud_filename)
+chunk.exportPoints(cloud_filename)
+
+# Save changes made to the metashape project
+doc.save(METASHAPE_PROJECT_PATH)
--- src/test_script.py
+++ src/test_script.py
@@ -6,11 +6,9 @@ import argparse
 import os
 import sys
 
-# This script will be passed:
-# beginning and end indexes of photo frame numbers to align / use
-# filepath of where to save the result
+# test script for making a dense point cloud
+# uses images from beginner doll tutorial
 
-# adapting this thing to the doll photoset
 first_frame = 1000
 last_frame = 1059
 frame_filepath = "/home/tanner/Downloads/doll/"
@@ -21,43 +19,14 @@ frame_list = []
 for current_frame_num in range(first_frame, last_frame):
     current_frame_filepath = frame_filepath + "IMG_" + str(current_frame_num) + ".JPG"
     frame_list.append(current_frame_filepath)
-    
-chunk.addCamera()
+
+#chunk.addCamera()
 chunk.addPhotos(frame_list)
+chunk.matchPhotos()
 chunk.alignCameras()
+chunk.buildDepthMaps()
 chunk.buildDenseCloud()
-
+doc.save("/home/tanner/Desktop/test.psx")
 # Save generated dense cloud to same location as frames
-cloud_filename = frame_filepath + "cloud_" + str(frame_number) + ".obj"
-Metashape.exportPoints(cloud_filename)
-
-'''
-# Save passed arguments into variables
-first_frame = sys.argv[1]
-last_frame = sys.argv[2]
-frame_filepath = sys.argv[3]
-
-# Set up Metashape document and chunk
-# going to be using a project "test.psx" that already exists and has
-# one chunk available for use
-doc = Metashape.Document()
-doc.open("/home/tanner/code/metashape_node/project/test.psx")
-chunk = doc.chunks()
-
-# Build list of filenames
-frame_list = []
-for current_frame_num in range(first_frame, last_frame):
-    current_frame_filepath = frame_filepath + str(current_frame_num) + ".jpeg"
-    frame_list.append(current_frame_filepath)
-
-# Add frame_list to chunk in metashape
-chunk.addPhotos(frame_list)
-
-# Align cameras and build dense cloud
-chunk.alignCameras()
-chunk.buildDenseCloud()
-
-# Save generated dense cloud to same location as frames
-cloud_filename = frame_filepath + "cloud_" + str(frame_number) + ".obj"
-Metashape.exportPoints(cloud_filename)
-'''
+#cloud_filename = frame_filepath + "cloud_" + str(frame_number) + ".obj"
+#Metashape.exportPoints(cloud_filename)
